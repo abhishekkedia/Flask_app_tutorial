bd32bbe2581fa3dd82dc82c287d00615cf1b37ff
abhishekkedia <abhishek1990@hotmail.com>
2024-06-08 15:49:15 +0100

    adding user input escaping

--- hello.py
+++ hello.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+from markupsafe import escape
+
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
