69967d5d60d7a413fd4b971cdf2848666474f6be
abhishekkedia <abhishek1990@hotmail.com>
2024-06-08 18:52:07 +0100

    Allowing Python to run these files in db.py and registering new functions with app

--- flaskr/db.py
+++ flaskr/db.py
@@ -21,7 +21,6 @@ def close_db(e=None):
 
 def init_db():
     db = get_db()
-
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
@@ -30,3 +29,7 @@ def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
     click.echo('Initialized the database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
